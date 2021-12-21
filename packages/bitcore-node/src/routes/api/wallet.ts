c426244759ac2dcc16618cbecf479a76d57506fe
Terlibin Oleg <rumblefishinfo@gmail.com>
2021-12-21 17:09:26 +0300

    lint

--- packages/bitcore-node/src/routes/api/wallet.ts
+++ packages/bitcore-node/src/routes/api/wallet.ts
@@ -9,7 +9,7 @@ function isTooLong(field, maxLength = 255) {
 }
 
 function isConnectionError(err: any): boolean {
-  return Boolean(err.message && err.message === 'connection not open'); 
+  return Boolean(err.message && err.message === 'connection not open');
 }
 // create wallet
 router.post('/', async function(req, res) {
@@ -147,11 +147,10 @@ router.get('/:pubKey/balance', Auth.authenticateMiddleware, async (req: Authenti
     });
     return res.send(result || { confirmed: 0, unconfirmed: 0, balance: 0 });
   } catch (err) {
-    
-    if ( isConnectionError(err) ) {
+    if (isConnectionError(err)) {
       return res.status(503).json(err);
     }
-    
+
     return res.status(500).json(err);
   }
 });
