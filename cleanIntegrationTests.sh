5e5278303399d78cd567fcc7fdaac6e5de3ed73b
Terlibin Oleg <rumblefishinfo@gmail.com>
2023-07-03 11:51:02 +0300

    Change docker files

--- cleanIntegrationTests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker container stop $(docker container ls -qf name=ducatuscore_*)
-docker container rm $(docker container ls -aqf name=ducatuscore_*)
-docker image rm ducatuscore-test_runner
-docker image rm ducatuscore-rippled
-$(dirname "$(readlink -f "$0")")/packages/ducatuscore-client/bin/wallet-delete --name EthereumWallet-Ci
-$(dirname "$(readlink -f "$0")")/packages/ducatuscore-client/bin/wallet-delete --name DucxWallet-Ci
