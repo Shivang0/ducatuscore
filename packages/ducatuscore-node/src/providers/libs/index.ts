cd01e97c5f956b67aa4651b1074420aa784bd352
Terlibin Oleg <rumblefishinfo@gmail.com>
2023-07-03 17:03:22 +0300

    Ducatuscore-node: change lib provider

--- packages/ducatuscore-node/src/providers/libs/index.ts
+++ packages/ducatuscore-node/src/providers/libs/index.ts
@@ -3,6 +3,10 @@ class LibProvider {
     BTC: {
       lib: require('@ducatus/ducatuscore-lib'),
       p2p: require('@ducatus/ducatuscore-p2p')
+    },
+    DUC: {
+      lib: require('@ducatus/ducatuscore-lib-duc'),
+      p2p: require('@ducatus/ducatuscore-p2p-duc')
     }
   };
 
